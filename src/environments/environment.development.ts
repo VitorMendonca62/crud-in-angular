b4b51aa769790bf77587ceac60a5f09aef9b365d
VitorMendonca62 <vitorqueiroz325@gmail.com>
2024-06-09 23:12:25 -0300

    .

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,3 @@
 export const environment = {
   hostApiUrl: 'http://localhost:3000',
-  secret: '188FCC7B02B2B02D9A561FB9459259F5',
-  expiresIn: '5D',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,1 @@
-export const environment = { hostApiUrl: '', secret: "188FCC7B02B2B02D9A561FB9459259F5", expiresIn: "5D" };
+export const environment = { hostApiUrl: 'http://localhost:3000' };
